4168f75980d7bceb2cee11b135e87b744e812c27
priyankarazdan08 <priyankarazdan@icloud.com>
2024-11-19 10:49:03 -0600

    asdf

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,9 +34,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="Instructions"
+        name="stats"
         options={{
-          title: 'Explore',
+          title: 'Instructions',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
